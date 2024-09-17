f4ff4c21fdcd68d13f09258db828396e1372f34c
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-09-17 15:26:24 +0300

    -add firebase -successfully fix the bugs and run project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:chatapp_mentor/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
